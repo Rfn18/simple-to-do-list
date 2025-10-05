225559aff8a7fe37d6ebd5ad7c7c3a91a80ed317
Rfn18 <rinofaster89@gmail.com>
2025-10-05 11:37:43 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { use, useState } from "react"
+
+function ToDoList() {
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState("")
+
+  function onChangebutton() {
+    
+  }
+
+  function newTask() {
+    
+  }
+
+    return ( 
+      <>
+        <h1>To Do List App</h1>
+      </>
+    )
+}
+
+export default ToDoList;   
